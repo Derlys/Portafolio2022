d1e35816efedb92d5b78b73558811b1aee8c9e09
Derlys <58484607+Derlys@users.noreply.github.com>
2022-05-15 14:29:14 +0100

    add-pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,28 @@ import { LayoutComponent } from "./ui/layout/layout.component";
 const routes: Routes = [
   {
     path: "",
+    // If this path is the 'full' match...
+    pathMatch: "full",
+    // ...redirect to this route.
+    redirectTo: "about"
+  },
+  {
+    path: "",
     component: LayoutComponent,
-    children: []
+    children: [
+      {
+        path: "about",
+        loadChildren: () =>
+          import("./pages/about/about.module").then((m) => m.AboutModule)
+      },
+      {
+        path: "portfolio",
+        loadChildren: () =>
+          import("./pages/portfolio/portfolio.module").then(
+            (m) => m.PortfolioModule
+          )
+      }
+    ]
   }
 ];
 
